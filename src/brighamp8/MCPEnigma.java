4a3a22d0746ee8663d7916d3049d4e5cfc7d7511
William Brigham <wsbrigham@msn.com>
2018-04-26 12:42:48 -0600

    Initial project creation.

---
+++ src/brighamp8/MCPEnigma.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brighamp8;
+
+/**
+ *
+ * @author WSBRI
+ */
+public class MCPEnigma extends Enigma{
+    private String localCodedMessage;
+    private int localKey = 1;
+    
+    
+    public MCPEnigma(){
+        
+        super();
+        
+        localCodedMessage = "";
+        localKey = 1;
+    }
+    
+    private void encode(){
+        
+        
+    }
+    
+    private void decode(){
+        
+    }
+
+    @Override
+     public void setMessage(String message){
+         
+         super.setMessage(message);
+         localCodedMessage = super.getCodedMessage();
+         localKey = super.getKey();
+         encode();
+         
+     }
+     
+     @Override
+    public void setMessage(String message, int key){
+        super.setMessage(message, key);
+         localCodedMessage = super.getCodedMessage();
+         localKey = super.getKey();
+         encode();
+         localCodedMessage = message;
+         localKey = key;
+         decode();
+         super.setCodedMessage(localCodedMessage, localKey);
+         
+        
+    }
+     
+    @Override
+    public void setCodedMessage(String codedMessage, int key){
+        
+    } 
+    
+    @Override 
+    public String getCodedMessage(){
+        
+        return localCodedMessage;
+        
+    }
+    
+    @Override
+     public int getKey(){
+         
+         return localKey;
+     }
+
+}
